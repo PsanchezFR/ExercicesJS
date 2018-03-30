28de4ad70b538ae675da817fffe1692b800ffaab
cpt <p.sanchez.fr@gmail.com>
2018-03-30 16:49:25 +0200

    jquery - ex2 - polished ex 3 - added files

--- JQUERY/ex2/ex2.js
+++ JQUERY/ex2/ex2.js
@@ -3,7 +3,10 @@ $(function(){
     // au survol des balises <a>, je veux qu'une infobulle soit créée
     // et affiche le contenu de l'attribut "title" du a en question
     $('a').on('mouseenter', function(e){
-        $('body').children('div').remove();
-        $('body').append('<div>' + $(this).attr('title') + '</div>');
+        $(this).append('<div>' + $(this).attr('title') + '</div>');
     });
+
+    $('a').on('mouseleave', function(e){
+        $(this).children('div').remove();
+    })
 });
